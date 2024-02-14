27979d6a2b53303ddb659bc48bda49b61916fae0
phichaiyut001 <pichayutkingpila@gmail.com>
2024-02-14 13:56:15 +0700

    scrape with cheerio

---
+++ index.js
@@ -0,0 +1,29 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+// url ที่ต้องการทำ scraping
+const url = "https://example.com";
+
+// ใช้ axios ดึง HTML  จาก url
+axios
+  .get(url)
+  .then((response) => {
+    //เก็บ HTML จาำ response
+    const html = response.data;
+
+    //ใช้ API cheerio เพื่อโหลด HTML
+    const load = cheerio.load(html);
+    // เลือกข้อมูลที่ต้องการจาก HTML โดยใช้ CSS selector
+    const title = load("title").text();
+    const p = [];
+    load("p").each((index, element) => {
+      p.push(load(element).text());
+    });
+
+    //แสดงผลลัพธ์
+    console.log("Title:", title);
+    console.log("P", p);
+  })
+  .catch((error) => {
+    console.log("Error fetching page", error);
+  });
